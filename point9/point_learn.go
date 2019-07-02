a1660a1fdc9ad9b591bdb9b8d0b35bef83a5fed8
yuanjz <yuanjz@deepblueai.com>
2019-07-02 18:34:38 +0800

    切片

---
+++ point9/point_learn.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "reflect"
+)
+
+var a string="1"
+
+var b *string=&a
+
+
+func main() {
+  fmt.Println(a,*b)
+  fmt.Printf("a type: %T\n", a)
+  fmt.Printf("b type: %T\n", b)
+  fmt.Printf("*b type: %T\n", *b)
+ fmt.Println(reflect.TypeOf(b).String())
+
+
+}
---
+++ slice/slice_learn.go
@@ -0,0 +1,7 @@
+package main
+
+var identifier []type
+func main() {
+
+  
+}
